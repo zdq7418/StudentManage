193798caf5f2df46d4bb30caad5f173bb380fb53
Sunsi <945222136@qq.com>
2017-04-23 13:14:08 +0800

    更新

---
+++ src/com/student/util/DataIdentification.java
@@ -0,0 +1,15 @@
+package com.student.util;
+/**
+ * 静态调用
+ * @author Sunsi
+ *
+ */
+public class DataIdentification {
+/**
+ * 用户状态
+ */
+	public final  static String USER_STATE_EXIT="01"; //已退出
+	public final  static String USER_STATE_LOGIN="02"; //已登录
+	public final  static String USER_STATE_TIMEOUT="03"; //超时中
+	public final  static String USER_STATE_DONOTUSE="04"; //不可用
+}
